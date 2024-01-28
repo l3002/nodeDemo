f270c87f19f1afd981924cccf6fc2da83c887e72
l3002 <tanmaymathpal4545@gmail.com>
2024-01-28 15:22:06 +0530

    feat: draw and winner functionality
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/Board.js
+++ src/Board.js
@@ -3,16 +3,56 @@ import SquareRow from "./SquareRow.js";
 import { useState } from "react";
 
 export default function Board() {
+  const [winner, setWinner] = useState(null);
   const [nextSelector, setSelector] = useState(0);
   const [values, setValue] = useState([
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ]);
+  const [dont, setDont] = useState(false);
 
-  function checkState(symbol) { }
+  function checkState() {
+    const a = [
+      [[0, 0], [0, 1], [0, 2]],
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
+    ];
+
+    for (let i = 0; i < 8; ++i) {
+      if (values[a[i][0][0]][a[i][0][1]]
+        && values[a[i][0][0]][a[i][0][1]] === values[a[i][1][0]][a[i][1][1]]
+        && values[a[i][0][0]][a[i][0][1]] === values[a[i][2][0]][a[i][2][1]]) {
+        setWinner(values[a[i][0][0]][a[i][0][1]]);
+      }
+    }
+  }
+
+  function onClickForReset() {
+    if (nextSelector < 9 && !winner) {
+      setDont(true);
+      return;
+    }
+    setSelector(0);
+    setDont(false);
+    setWinner(null);
+    setValue([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]);
+  }
 
   function handleClick(i, j) {
+    if (winner || values[i][j] !== null) {
+      return;
+    }
+    setDont(false);
     const nextValues = values.slice();
     if (nextSelector % 2 === 0) {
       nextValues[i][j] = "o";
@@ -21,12 +61,22 @@ export default function Board() {
     }
     setSelector(nextSelector + 1);
     setValue(nextValues);
+    checkState();
   }
 
   return (
-    <div className="App min-vh-100 d-flex justify-content-center align-items-center">
+    <div className="board">
+      <ThemeSwitcher />
+      {nextSelector === 9 && !winner ? <div>It's a Draw</div> : null}
+      {dont ? <div>Don't do that!!</div> : null}
+      {winner ? <div>Winner is {winner}</div> : null}
+      <div className="reset">
+        <button type="reset" onClick={onClickForReset}>Reset Table</button>
+      </div>
       <div>
-        <ThemeSwitcher />
+        <SquareRow rowValue={0} values={values} onClick={handleClick} />
+        <SquareRow rowValue={1} values={values} onClick={handleClick} />
+        <SquareRow rowValue={2} values={values} onClick={handleClick} />
       </div>
     </div>
   );
